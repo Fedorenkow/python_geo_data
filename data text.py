79a7dd0b02106ca745136d089ea9d8205de75ba6
Fedorenkow <oleh.yushchuk@gmail.com>
2023-04-04 15:00:29 +0300

    Create function FindPoint

--- data text.py
+++ data text.py
@@ -3,6 +3,13 @@ from rasterio.features import shapes
 import geopandas as gpd
 from shapely.geometry import Point
 
+def FindPoint(point):
+    gdf = gpd.read_file("field_centroids.geojson")
+    point = gdf.geometry.iloc[point]
+    x = point.x, point.y
+    print(x)
+    return x
+
 with rasterio.open('./soil_moisture.tif') as src:
     image = src.read(1)
     results = (
@@ -11,9 +18,7 @@ with rasterio.open('./soil_moisture.tif') as src:
         in enumerate(shapes(image, mask=None, transform=src.transform))
     )
 
-gdf = gpd.read_file("field_centroids.geojson")
-point=gdf.geometry.iloc[0]
-x = point.x,point.y
-print(x)
+point = FindPoint(0)
+print(point)
 
 
